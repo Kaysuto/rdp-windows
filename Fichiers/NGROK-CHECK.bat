a890434ce23010d4e998b273aa994967b6689fb0
Kaysuto <contact@kaysuto.fr>
2022-09-25 03:26:37 +0200

    Update NGROK-CHECK.bat

--- Fichiers/NGROK-CHECK.bat
+++ Fichiers/NGROK-CHECK.bat
@@ -6,7 +6,8 @@ net user runneradmin P@ssw0rd! /add >nul
 net localgroup administrators runneradmin /add >nul
 echo C'est fait ! Connectez votre VM en utilisant RDP. Lorsque le RDP a expiré et que la VM s'est arrêtée, relancez les tâches pour obtenir un nouveau RDP.
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbRyFTLRNyDmT1a1boZVcorrect in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Impossible d'obtenir le tunnel NGROK, assurez-vous que NGROK_AUTH_TOKEN est correct dans Settings > Secrets > Repository secret. Peut-être que votre VM précédente fonctionne toujours : https://dashboard.ngrok.com/status/tunnels " 
+
 echo User: runneradmin
 echo Pass: P@ssw0rd!
 curl -O https://raw.githubusercontent.com/aloksharmakumar77/Windows2019RDP-US/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
@@ -18,3 +19,4 @@ sc config Audiosrv start= auto >nul
 ICACLS C:\Windows\Temp /grant runneradmin:F >nul
 ICACLS C:\Windows\installer /grant runneradmin:F >nul
 ping -n 10 127.0.0.1 >nul
+
